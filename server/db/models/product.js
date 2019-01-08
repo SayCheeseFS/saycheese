ee8ce2cf0463c187a39ee52497dfc3a1138c316e
dmitry-shmakov <shmakov.dim@gmail.com>
2019-01-08 17:17:10 -0500

    Fixed the bug with Sequelize.Array

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -2,43 +2,41 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Product = db.define('product', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    productType: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    description: {
-        type: Sequelize.TEXT
-    },
-    images: {
-        type: Sequelize.ARRAY,
-        defaultValue: 'no_picture.jpg'
-    },
-    price: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    },
-    inStock: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: true,
-        allowNull: false
-    },
-    brand: {
-        type:Sequelize.STRING,
-        allowNull: false
-    },
-    rating: {
-        type: Sequelize.INTEGER,
-        defaultValue: null
-    },
-    discount: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0
-    }
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  productType: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  images: {
+    type: Sequelize.ARRAY(Sequelize.TEXT)
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  inStock: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: true,
+    allowNull: false
+  },
+  brand: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  rating: {
+    type: Sequelize.INTEGER,
+    defaultValue: null
+  },
+  discount: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  }
 })
-
 
 module.exports = Product
