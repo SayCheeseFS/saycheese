60c9ac8f03112e6fd9461baf992dbb4ae9650c94
Sahil Pabby <pabs124@gmail.com>
2019-01-08 16:56:41 -0500

    made routes for products

---
+++ server/api/product.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const {Product} = require('../db/models')
+module.exports = router
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.productId)
+    res.json(product)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll({
+      // explicitly select only the id and email fields - even though
+      // users' passwords are encrypted, it won't help if we just
+      // send everything to anyone who asks!
+    })
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
