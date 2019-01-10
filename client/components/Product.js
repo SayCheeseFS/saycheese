2deea6e0411670a869b3b51662e285254feaa36a
dmitry-shmakov <shmakov.dim@gmail.com>
2019-01-10 11:16:38 -0500

    Resolved Single Product view

--- client/components/Product.js
+++ client/components/Product.js
@@ -1,26 +1,35 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {fetchSingleProduct} from '../store'
 
-const Product = ({product}) => {
-  return (
-    <div>
-      <img src={product.images[0]} />
-      <h1>{product.name}</h1>
-      <h3>{product.price}</h3>
-      <h4>{product.description}</h4>
-      <h4>Rating: {product.rating}</h4>
-      <h4>In Stock</h4>
-      <table>
-        <tr>
-          <td>
-            <button>Add To Cart</button>
-          </td>
-        </tr>
-      </table>
-    </div>
-  )
+class Product extends React.Component {
+  componentDidMount() {
+    // console.log(this.props, this.props.match)
+    this.props.fetchSingleProduct(this.props.match.params.id)
+  }
+  render() {
+    const product = this.props.product
+    return (
+      <div>
+        <img src={`/images/${product.imageUrl}`} />
+        <h1>{product.name}</h1>
+        <h3>{product.price}</h3>
+        <h4>{product.description}</h4>
+        <h4>Rating: {product.rating}</h4>
+        <h4>In Stock</h4>
+        <button type="submit">Add To Cart</button>
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = ({product}) => ({product})
+const mapStateToProps = state => ({product: state.inventory.product})
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchSingleProduct: id => {
+      dispatch(fetchSingleProduct(id))
+    }
+  }
+}
 
-export default connect(mapStateToProps, null)(Product)
+export default connect(mapStateToProps, mapDispatchToProps)(Product)
