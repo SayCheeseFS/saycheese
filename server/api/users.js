deeab01717e4d92748dc391d020b9677233b5107
dmitry-shmakov <shmakov.dim@gmail.com>
2019-01-10 15:31:40 -0500

    Fixed the route and a bug with adding the same product into cart

--- server/api/users.js
+++ server/api/users.js
@@ -2,23 +2,27 @@ const router = require('express').Router()
 const {User, Order_Product, Order, Product} = require('../db/models')
 module.exports = router
 
-router.post('/:userId/cart', async (req, res, next) => {
+router.post('/cart', async (req, res, next) => {
   let user = {}
   try {
     // check if there is a user
     if (!req.user) {
       user = await User.create()
       req.login(user, err => (err ? next(err) : null))
+    } else {
+      user = req.user
     }
     // check if there is a cart
     const cart = await Order.findOrCreate({
       where: {userId: user.id, isCart: true}
     })
     const newItem = await Product.findById(req.body.productId)
-    await Order_Product.create({
-      orderId: cart[0].id,
-      productId: newItem.id,
-      price: newItem.price
+    await Order_Product.findOrCreate({
+      where: {
+        orderId: cart[0].id,
+        productId: newItem.id,
+        price: newItem.price
+      }
     })
     res.send(cart)
   } catch (err) {
