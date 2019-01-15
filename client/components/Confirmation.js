58e9213b6d2e35fb32d3273381e54464668ce7c4
Sahil Pabby <pabs124@gmail.com>
2019-01-15 16:57:59 -0500

    finished styling

--- client/components/Confirmation.js
+++ client/components/Confirmation.js
@@ -2,15 +2,24 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 class Confirmation extends React.Component {
-  componentDidUpdate() {}
-
   render() {
     return (
       <div>
-        <h2>Your Order Has Been Placed!</h2>
-        <Link to="/">
-          <button>Continue Shopping</button>
-        </Link>
+        <div className="ui card align centered">
+          <div className="content">
+            <div className="header">
+              <h2>Thank You.</h2>
+            </div>
+          </div>
+          <div className="content">
+            <h2 className="ui sub header">Your Order Has Been Placed :)</h2>
+          </div>
+          <div className="extra content">
+            <Link to="/">
+              <button className="ui button">Continue Shopping</button>
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }
